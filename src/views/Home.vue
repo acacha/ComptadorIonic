aca6a6ce4e667926c75e10ec8f6be3432d8bb481
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-11-09 10:19:03 +0100

    Versió inicial codi scaffolding del comptador TimeFighter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Time fighter</ion-title>
       </ion-toolbar>
     </ion-header>
     
@@ -14,8 +14,8 @@
       </ion-header>
     
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Yes: TODO AFEGIR EL BOTÓ DEL COMPTADOR</strong>
+        <p>PROVA<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
